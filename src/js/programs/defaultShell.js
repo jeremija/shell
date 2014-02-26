ec16ac27a9d13c617e0f995634da39bcbbf7156b
jeremija <jerko.steiner@gmail.com>
2014-02-26 09:25:53 +0100

    Changed default shell intro message

--- src/js/programs/defaultShell.js
+++ src/js/programs/defaultShell.js
@@ -7,9 +7,22 @@ define(['cli/Shell', 'events/events'], function(Shell, events) {
         name: '',
         default: function() {
             this.output(new Date());
-            this.output('Shell initialized, type "help" and press enter for instructions');
+            this.output(' ');
+            this.output('Hello, stranger!');
+            this.output(' ');
+            this.output('This is a unix-like shell I made to showcase my programming skills.');
+            this.output('Here you can find information about my background, skills, work experience and more.');
+            this.output(' ');
+            this.output('Type "help" and press enter to get started. Follow the instructions along the way.');
+            this.output(' ');
+            this.output('Enjoy your stay!');
+            this.output('    Jerko Steiner');
+            this.output(' ');
         },
         commands: {
+            'intro': function() {
+                this.default();
+            },
             'clear': function(arg) {
                 if (arg === '--help') {
                     this.output('clear: clears the console output');
